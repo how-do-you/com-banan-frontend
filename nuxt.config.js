7ada93c24bc5057b036bc24c7d66f4aa018a6a00
Simon Hyll <simon@adaptivehedge.com>
2022-06-07 21:28:12 +0200

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,7 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  distDir: 'dist',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
